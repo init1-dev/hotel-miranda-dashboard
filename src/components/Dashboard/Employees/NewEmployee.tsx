df2fad0755ad084481d55a7ea981d49a065b6b35
init1-dev <init1.dev@gmail.com>
2024-03-10 03:29:57 +0100

    ADD: new employee template form

--- src/components/Dashboard/Employees/NewEmployee.tsx
+++ src/components/Dashboard/Employees/NewEmployee.tsx
@@ -1,7 +1,111 @@
+import styled from "styled-components";
+
 function NewEmployee () {
     return (
-        <>NEW EMPLOYEE</>
+        <>
+            <Title>NEW EMPLOYEE</Title>
+
+            <Form id="new-room" name="new-room" action="">
+                <GridContainer>
+                    <Label htmlFor="room-type">Room Type:</Label>
+                    <Select name="room-type" id="room-type" required>
+                        <option value="" selected hidden>Click to see options</option>
+                        {
+                            // roomTypes.map((type, index) => {
+                            //     return <option key={index} value={type.value}>{type.label}</option>
+                            // })
+                        }
+                    </Select>
+
+                    <Label htmlFor="room-number">Room Number:</Label>
+                    <Input type="number" name="room-number" id="room-number" value="" placeholder="Room number" required/>
+
+                    <Label htmlFor="description">Description:</Label>
+                    <TextArea name="description" id="description" cols={30} rows={10} placeholder="Insert room description" required>
+                    </TextArea>
+
+                    <Label htmlFor="cancelation-policy">Cancelation Policy:</Label>
+                    <TextArea name="cancelation-policy" id="cancelation-policy" cols={30} rows={10} placeholder="Insert cancelation policy" required>
+                    </TextArea>
+                </GridContainer>
+
+                <GridContainer>
+                    <Label htmlFor="files">Load Images:</Label>
+                    <Input type="file" name="files" id="files"/>
+
+                    <Label htmlFor="offer">Offer:</Label>
+                    <Input type="number" name="offer" id="offer" value="" placeholder="Insert offer price"/>
+
+                    <Label htmlFor="price">Price:</Label>
+                    <Input type="number" name="price" id="price" value="" placeholder="Insert price" required/>
+
+                    
+
+                    <Label htmlFor="amenities">Amenities:</Label>
+                    <AmenitiesSelect multiple name="amenities" id="amenities" required>
+                        {
+                            // amenities.map((type, index) => {
+                            //     return <option key={index} value={type.value}>{type.label}</option>
+                            // })
+                        }
+                    </AmenitiesSelect>
+                    <Button type="submit" form="new-room">
+                        Submit
+                    </Button>
+                </GridContainer>
+            </Form>
+        </>
     );
 }
+
+const Title = styled.h1`
+    margin: 0 15% 0 15%;
+`
+
+const Form = styled.form`
+    background-color: ${({ theme }) => theme.contentBg};
+    padding: 2rem;
+    border-radius: 0.5rem;
+    display: grid;
+    justify-items: center;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 1rem 15% 0 15%;
+    box-shadow: 1px 1px 5px black;
+`
+
+const GridContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 90%;
+`
+
+const Label = styled.label`
+    display: block;
+`
+
+const Input = styled.input`
+    display: block;
+    width: 100%;
+    padding: 0.5rem;
+`
+
+const TextArea = styled.textarea`
+    width: 100%;
+    padding: 0.5rem;
+`
+
+const Select = styled.select`
+    width: 100%;
+    padding: 0.5rem;
+`
+
+const AmenitiesSelect = styled(Select)`
+    height: 245px;
+`
+
+const Button = styled.button`
+    color: white;
+`
 
 export default NewEmployee ;
