d587c20feaeff3417855a7641889bbf43ba4bbcb
init1-dev <init1.dev@gmail.com>
2024-03-20 18:37:47 +0100

    ADD: Authentication spec complete, all aserts OK and helper functions for login and badLogin

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://192.168.2.168:5173/",
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,7 +1,31 @@
 /// <reference types="cypress" />
+import { badLogin, login } from '../../src/helpers/cypress/functions'
 
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
-  })
+describe('Authentication spec', () => {
+
+    it('successfuly visit login page', () => {
+        cy.visit('')
+        cy.url().should('include', "/login")
+
+        cy.visit('/dashboard')
+        cy.url().should('include', "/login")
+
+        badLogin()
+        cy.url().should('include', "/login")
+
+        login()
+        cy.url().should('include', "/dashboard")
+
+        cy.visit('/login')
+        cy.url().should('include', "/dashboard")
+
+        cy.get('button').contains("🌙").click()
+        cy.contains('h4', 'Dashboard').invoke('css', 'color').should('equal', 'rgb(255, 255, 255)')
+
+        cy.get('button').contains("☀️").click()
+        cy.contains('h4', 'Dashboard').invoke('css', 'color').should('equal', 'rgb(0, 0, 0)')
+
+        cy.get('button').contains("Log out").click()
+        cy.url().should('include', "/login")
+    })
 })
